41e70e4afdd0a34c5317b68e3a075a128e95298a
Tobias Deiminger <tobias.deiminger@linutronix.de>
2026-09-01 17:29:51 +0200

    test: replace pkcs11-tool with p11tool in PKI enrollment
    pkcs11-tool (opensc) and p11tool (gnutls-bin) have comparable
    functionality. p11tool is already used by the production code and
    support PKCS11 URIs. Thus replace pkcs11-tool entirely.
    
    Signed-off-by: Tobias Deiminger <tobias.deiminger@linutronix.de>
    Reviewed-by: Florian Kauer <florian.kauer@linutronix.de>

--- test/scripts/enroll_test_pki.sh
+++ test/scripts/enroll_test_pki.sh
@@ -4,8 +4,8 @@
 #
 # SPDX-License-Identifier: 0BSD
 
-export PKCS11_MODULE=/usr/lib/softhsm/libsofthsm2.so
 export TOKEN=SoftHSM
+export GNUTLS_PIN=1234
 
 function cleanup {
   rm -rf "$tmpdir"
@@ -20,68 +20,68 @@ cd "$tmpdir"
 touch ./index.txt
 echo "12345678" > ./serial
 
+function keygen {
+  p11tool --login --generate-rsa --bits 4096 --label "$1" --outfile /dev/null "pkcs11:token=${TOKEN}"
+}
+
+function write_cert {
+  p11tool --login --write --load-certificate "$1" --label "$2" "pkcs11:token=${TOKEN}"
+}
+
 # Generating CA key and certificate
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habCA1"
+keygen "habCA1"
 openssl req -engine pkcs11 -new -batch -subj /CN=CA1_sha256_4096_65537_v3_ca/ -key "pkcs11:token=${TOKEN};object=habCA1" -keyform engine -out "${tmpdir}/crts/CA1_sha256_4096_65537_v3_ca_crt.pem" -text -x509 -extensions v3_ca -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/CA1_sha256_4096_65537_v3_ca_crt.pem" -out "${tmpdir}/crts/CA1_sha256_4096_65537_v3_ca_crt.der"
 
 # Generating SRK key and certificate 1
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habSRK1CA"
+keygen "habSRK1CA"
 openssl req -engine pkcs11 -new -batch -subj /CN=SRK1_sha256_4096_65537_v3_ca/ -keyform engine -key "pkcs11:token=${TOKEN};object=habSRK1CA" -out temp_srk_req.pem -passin pass:1234
 openssl ca -engine pkcs11 -batch -md sha256 -outdir ./ -in ./temp_srk_req.pem -cert "${tmpdir}/crts/CA1_sha256_4096_65537_v3_ca_crt.pem" -keyform engine -keyfile "pkcs11:token=${TOKEN};object=habCA1" -extfile /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/v3_ca.cnf -out "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.pem" -notext -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.pem" -out "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.der" --type cert --label "habSRK1CA" --pin 1234
+write_cert "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.pem" "habSRK1CA"
 
 # Generating CSF key and certificate 1
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habCSF11"
+keygen "habCSF11"
 openssl req -engine pkcs11 -new -batch -subj /CN=CSF1_1_sha256_4096_65537_v3_usr/ -keyform engine -key "pkcs11:token=${TOKEN};object=habCSF11" -out temp_csf_req.pem -passin pass:1234
 openssl ca -engine pkcs11 -batch -md sha256 -outdir ./ -in ./temp_csf_req.pem -cert "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.pem" -keyform engine -keyfile "pkcs11:token=${TOKEN};object=habSRK1CA" -extfile /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/v3_usr.cnf -out "${tmpdir}/crts/CSF1_1_sha256_4096_65537_v3_usr_crt.pem" -notext -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/CSF1_1_sha256_4096_65537_v3_usr_crt.pem" -out "${tmpdir}/crts/CSF1_1_sha256_4096_65537_v3_usr_crt.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/CSF1_1_sha256_4096_65537_v3_usr_crt.der" --type cert --label "habCSF11" --pin 1234
+write_cert "${tmpdir}/crts/CSF1_1_sha256_4096_65537_v3_usr_crt.pem" "habCSF11"
 
 # Generating IMG key and certificate 1
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habIMG11"
+keygen "habIMG11"
 openssl req -engine pkcs11 -new -batch -subj /CN=IMG1_1_sha256_4096_65537_v3_usr/ -keyform engine -key "pkcs11:token=${TOKEN};object=habIMG11" -out temp_img_req.pem -passin pass:1234
 openssl ca -engine pkcs11 -batch -md sha256 -outdir ./ -in ./temp_img_req.pem -cert "${tmpdir}/crts/SRK1_sha256_4096_65537_v3_ca_crt.pem" -keyform engine -keyfile "pkcs11:token=${TOKEN};object=habSRK1CA" -extfile /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/v3_usr.cnf -out "${tmpdir}/crts/IMG1_1_sha256_4096_65537_v3_usr_crt.pem" -notext -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/IMG1_1_sha256_4096_65537_v3_usr_crt.pem" -out "${tmpdir}/crts/IMG1_1_sha256_4096_65537_v3_usr_crt.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/IMG1_1_sha256_4096_65537_v3_usr_crt.der" --type cert --label "habIMG11" --pin 1234
+write_cert "${tmpdir}/crts/IMG1_1_sha256_4096_65537_v3_usr_crt.pem" "habIMG11"
 
 # Generating SRK key and certificate 2
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habSRK2CA"
+keygen "habSRK2CA"
 openssl req -engine pkcs11 -new -batch -subj /CN=SRK2_sha256_4096_65537_v3_ca/ -keyform engine -key "pkcs11:token=${TOKEN};object=habSRK2CA" -out temp_srk_req.pem -passin pass:1234
 openssl ca -engine pkcs11 -batch -md sha256 -outdir ./ -in ./temp_srk_req.pem -cert "${tmpdir}/crts/CA1_sha256_4096_65537_v3_ca_crt.pem" -keyform engine -keyfile "pkcs11:token=${TOKEN};object=habCA1" -extfile /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/v3_ca.cnf -out "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.pem" -notext -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.pem" -out "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.der" --type cert --label "habSRK2CA" --pin 1234
+write_cert "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.pem" "habSRK2CA"
 
 # Generating CSF key and certificate 2
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habCSF21"
+keygen "habCSF21"
 openssl req -engine pkcs11 -new -batch -subj /CN=CSF2_1_sha256_4096_65537_v3_usr/ -keyform engine -key "pkcs11:token=${TOKEN};object=habCSF21" -out temp_csf_req.pem -passin pass:1234
 openssl ca -engine pkcs11 -batch -md sha256 -outdir ./ -in ./temp_csf_req.pem -cert "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.pem" -keyform engine -keyfile "pkcs11:token=${TOKEN};object=habSRK2CA" -extfile /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/v3_usr.cnf -out "${tmpdir}/crts/CSF2_1_sha256_4096_65537_v3_usr_crt.pem" -notext -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/CSF2_1_sha256_4096_65537_v3_usr_crt.pem" -out "${tmpdir}/crts/CSF2_1_sha256_4096_65537_v3_usr_crt.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/CSF2_1_sha256_4096_65537_v3_usr_crt.der" --type cert --label "habCSF21" --pin 1234
+write_cert "${tmpdir}/crts/CSF2_1_sha256_4096_65537_v3_usr_crt.pem" "habCSF21"
 
 # Generating IMG key and certificate 2
 
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "habIMG21"
+keygen "habIMG21"
 openssl req -engine pkcs11 -new -batch -subj /CN=IMG2_1_sha256_4096_65537_v3_usr/ -keyform engine -key "pkcs11:token=${TOKEN};object=habIMG21" -out temp_img_req.pem -passin pass:1234
 openssl ca -engine pkcs11 -batch -md sha256 -outdir ./ -in ./temp_img_req.pem -cert "${tmpdir}/crts/SRK2_sha256_4096_65537_v3_ca_crt.pem" -keyform engine -keyfile "pkcs11:token=${TOKEN};object=habSRK2CA" -extfile /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/v3_usr.cnf -out "${tmpdir}/crts/IMG2_1_sha256_4096_65537_v3_usr_crt.pem" -notext -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/IMG2_1_sha256_4096_65537_v3_usr_crt.pem" -out "${tmpdir}/crts/IMG2_1_sha256_4096_65537_v3_usr_crt.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/IMG2_1_sha256_4096_65537_v3_usr_crt.der" --type cert --label "habIMG21" --pin 1234
+write_cert "${tmpdir}/crts/IMG2_1_sha256_4096_65537_v3_usr_crt.pem" "habIMG21"
 
 # Generating TA root key / public key
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "ta-root-key"
+keygen "ta-root-key"
 
 # Generating Rpi root key / public key
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "rpi-boot-key"
+keygen "rpi-boot-key"
 
 # Generating SWU key and certificate
-pkcs11-tool --module $PKCS11_MODULE -l --pin 1234 --keypairgen --key-type rsa:4096 --label "SWU"
+keygen "SWU"
 openssl req -engine pkcs11 -new -batch -subj /CN=CA1_sha256_4096_65537_v3_ca/ -key "pkcs11:token=${TOKEN};object=SWU" -keyform engine -out "${tmpdir}/crts/swu_crt.pem" -text -x509 -extensions v3_ca -days 1825 -config /usr/share/doc/imx-code-signing-tool/pki_scripts/ca/openssl.cnf -passin pass:1234
-openssl x509 -outform DER -in "${tmpdir}/crts/swu_crt.pem" -out "${tmpdir}/crts/swu_crt.pem.der"
-pkcs11-tool --module $PKCS11_MODULE -l --write-object "${tmpdir}/crts/swu_crt.pem.der" --type cert --label "SWU" --pin 1234
+write_cert "${tmpdir}/crts/swu_crt.pem" "SWU"
